ff129d6876806c69800143d8e4dbe955765484e9
jonhks <jonathan@laboratoria.la>
2018-07-13 09:37:25 -0600

    intalando y modificando estilos de eslint

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 // Imputs
-const inputName = document.getElementById("icon_name");
+const inputName = document.getElementById('icon_name');
 const inputEmail = document.getElementById('icon_email');
-const inputPassword = document.getElementById('icon_password')
+const inputPassword = document.getElementById('icon_password');
 const inputPassword2 = document.getElementById('icon_password-2');
 const modalBox = document.getElementById('modal1');
 
@@ -11,23 +11,21 @@ const google = document.getElementById('google');
 const login = document.getElementById('login');
 const singup = document.getElementById('singup');
 
+google.addEventListener('click', getProvider);
 
 
-google.addEventListener("click", getProvider );
-
-
-singup.addEventListener("click", event =>{
+singup.addEventListener('click', event =>{
   let name = inputName.value;
   let email = inputEmail.value;
-  let pass=  inputPassword.value;
+  let pass = inputPassword.value;
   localStorage.clear();
-  localStorage.setItem("name", name);
-  localStorage.setItem("email", email);
-  localStorage.setItem("pass", pass);
+  localStorage.setItem('name', name);
+  localStorage.setItem('email', email);
+  localStorage.setItem('pass', pass);
   window.social.crearCuenta(
     name,
     email,
     pass
     // inputPassword2.value
   );
-})
+});
